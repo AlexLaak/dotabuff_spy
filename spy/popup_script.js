fe4e49f6064fa6fd16a5808d2e9e38fc6ab5bcb7
AlexLaak <allu__11@hotmail.com>
2021-09-14 23:17:42 +0300

    display whitelisted ids more nicely

--- spy/popup_script.js
+++ spy/popup_script.js
@@ -19,17 +19,31 @@ function onWhitelistPlayerIDSubmit()
     let whitelisted_id = document.getElementById("whitelisted_id").value;
     document.getElementById("whitelisted_id").value = "";
     whitelisted_ids.push(parseInt(whitelisted_id));
+
     let whitelistedids_obj = {"whitelisted_ids": whitelisted_ids};
-    document.getElementById("whitelisted_ids_display").innerHTML = whitelisted_ids;
     port.postMessage(whitelistedids_obj);
+
+    var list = document.getElementById('whitelisted_ids_list');
+    var entry = document.createElement('li');
+    entry.innerHTML = whitelisted_id;
+    list.appendChild(entry);
 }
 
+// This function gets called when the popup menu is opened
 function updateCurrentIDsViaBackend(data)
 {
     player_id = data.player_id;
     whitelisted_ids = data.whitelisted_ids;
     document.getElementById("player_id_display").innerHTML = player_id;
-    document.getElementById("whitelisted_ids_display").innerHTML = whitelisted_ids;
+    var list = document.getElementById('whitelisted_ids_list');
+
+    for (let index = 0; index < whitelisted_ids.length; index++)
+    {
+        const id = whitelisted_ids[index];
+        var entry = document.createElement('li');
+        entry.innerHTML = id
+        list.appendChild(entry);
+    }
 }
 
 var port = chrome.extension.connect({
